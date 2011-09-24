4913c0a096e5711eacb6cf20ece15be8d674c228
Jorge Paez <george.paez@gmail.com>
2011-09-24 18:34:39 -0500

    Initial commit

---
+++ app/admin/products.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register Product do
+  index do
+    column :name
+    column :category
+    column "Release Date", :released_at
+    column :price, :sortable => :price do |product|
+      div :class => "price" do
+        number_to_currency product.price
+      end
+    end
+    default_actions
+  end
+end
